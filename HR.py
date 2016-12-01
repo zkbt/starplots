ef31ef5f4b31c70ed639a989f17afb68cb7b700a
Zach Berta-Thompson <zkbt@kindness.colorado.edu>
2016-12-01 12:19:11 -0700

    add Hertzsprung-Russell (and more) diagrams

---
+++ HR.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load information about a bunch of stars
+try:
+    data = np.loadtxt('hipparcos.txt')
+except IOError:
+    data = np.loadtxt('/home/zabe0091/astr2600/')
+
+# this module comes from Dan Foreman-Mackey et al.,
+# at the github repo https://github.com/dfm/corner.py
+
+import corner
+corner.corner(data,
+              labels=['V-I (color)',
+                      'V (absolute)',
+                      'RA (deg)',
+                      'Dec (deg)',
+                      'distance (pc)'],
+              plot_density=False,
+              plot_contours=False)
+
+plt.show()
